bdebfc5fab34b43508a09ac1c9100cbe38f9c14d
Dima (IfLunatic) <dimasilenkoluna@gmail.com>
2023-11-15 01:36:28 +0200

    Initial commit

---
+++ main.py
@@ -0,0 +1,60 @@
+import time
+import os
+from selenium import webdriver
+from selenium.webdriver.chrome.service import Service
+from selenium.webdriver.common.by import By
+from selenium.webdriver.common.keys import Keys
+from selenium.webdriver.support.ui import WebDriverWait
+from selenium.webdriver.support import expected_conditions as EC
+from selenium.webdriver.support.ui import Select
+from selenium.webdriver.chrome.options import Options
+
+
+s = Service(executable_path='chromedriver-win64/chromedriver.exe')
+
+chrome_options = Options()
+chrome_options.add_argument("--user-data-dir=C:\\Users\\Dima (IfLunatic)\\AppData\\Local\\Google\\Chrome\\User Data\\Default")
+driver = webdriver.Chrome(service=s, options=chrome_options)
+
+
+try:
+    driver.maximize_window()
+    driver.get('https://getpass.civic.com/status?chain=polygon')
+    time.sleep(2)
+
+    WebDriverWait(driver, 10).until(
+        EC.visibility_of_element_located((By.XPATH, '//*[@id="headlessui-listbox-button-:r0:"]'))
+    ).click()
+
+    WebDriverWait(driver, 10).until(
+        EC.visibility_of_element_located((By.XPATH, "//span[text()='CAPTCHA Verification']"))
+    ).click()
+
+    WebDriverWait(driver, 10).until(
+        EC.visibility_of_element_located(
+            (By.XPATH, '//*[@id="root"]/div/div[1]/div[1]/div/div/button'))
+    ).click()
+    time.sleep(2)
+
+    WebDriverWait(driver, 10).until(
+        EC.visibility_of_element_located(
+            (By.XPATH, '/html/body/div[2]/div/div/div[2]/div/div/div/div/div[1]/div[2]/div[2]/div[3]/button'))
+    ).click()
+    time.sleep(2)
+
+    active_window = driver.window_handles[-1]
+    driver.switch_to.window(active_window)
+
+    password_input = WebDriverWait(driver, 10).until(
+        EC.visibility_of_element_located((By.XPATH, '//*[@id="password"]'))
+    )
+    password_input.send_keys("ваш_пароль")
+
+    time.sleep(5)
+
+
+except Exception as ex:
+    print(ex)
+finally:
+    driver.close()
+    driver.quit()
